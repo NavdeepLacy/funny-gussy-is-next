d44ae0f9cfd04108ec96fce2c280ef213ffdf157
NavdeepLacy <123606645+NavdeepLacy@users.noreply.github.com>
2023-01-26 02:17:29 +0000

    Initial commit

---
+++ footer.php
@@ -0,0 +1,22 @@
+<?php 
+
+echo "
+
+<footer>
+
+<ul>
+
+  <li><a href="privacy.html"> Privacy Policy </a></li>
+  <li><a href="terms.html"> Terms of Service </a></li>
+  <li><a href="changelog.html"> Donate </a></li>
+  
+
+</ul>
+
+<img id="funguloid" src="fungi4.png" />
+
+</footer>
+
+"
+
+?>
---
+++ index.php
@@ -0,0 +1,62 @@
+
+<?php
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link rel="stylesheet" href="style.css" />
+    <link rel="icon" type="image/x- icon" href="fungi2.png" />
+    <title>Fungus</title>
+    <script defer data-domain="fungusproject.ga" src="https://plausible.io/js/script.js"></script>
+
+  </head>
+    <script> if (/\bCrOS\b/.test(navigator.userAgent)) {
+        alert("IT IS NOT SAFE TO USE THIS WEBSITE ON A SCHOOL CHROMEBOOK!!! (we detected you are using chromeOS)");
+    } else {
+        
+    } 
+    </script> 
+    <style> .btn {
+      
+    } </style>
+  <body>
+    <div id="page-container">
+      <div id="content-wrap">
+        <div id="title">
+        <img id="textlogo" src="textlogo.png" />
+        <h3 class="centertext" id="black">
+          A group of disgruntled students that care about our privacy
+        </h3>
+      </div> 
+      
+      <div id="posts"> 
+      
+        <p> F.U.N.G.U.S. is a group of disgruntled students that fundamentally disagree with student surveillance and the removal of the BYOD network in Gwinnett County Schools. You can read more about us <a href="about.html"> here</a>. </p>
+      <p> We have information on how to bypass the school wifi ban, school restrictions on the wifi, and more. However, we encourage you to stick around and read up on why we're doing this, and how you can help. </p>
+      </div>
+      <div id="nav">
+
+          <h1> Important links </h1>
+          
+          
+            <div id="links">
+           <a class="btn" href="about.html"> About Us </a> 
+           <a class="btn" href="grievances.html"> List of Grievances </a> 
+          
+           <a class="btn" href="FAQ.html"> FAQ </a> 
+           <a class="btn" href="guest.html"> How to Access School WiFi </a> 
+           <a class="btn" href="blog.html"> Blog </a>
+           <a class="btn" href="docs.html"> Docs </a>
+         
+          
+            </div>
+      </div> 
+    </div>
+      </div>
+
+      <?php include "footer.php"; ?> 
+
+    </div>
+  </body>
+</html>
